db1059ffb36bfb14c3966f54a698ce9dc66aaf27
zhou <sohozhou@163.com>
2014-05-22 22:25:32 +0800

    vp first commit`

---
+++ src/com/example/trlab/vp/TabView.java
@@ -0,0 +1,17 @@
+package com.example.trlab.vp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class TabView extends LinearLayout{
+
+	public TabView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public TabView(Context context){
+		super(context);
+	}
+	
+}
