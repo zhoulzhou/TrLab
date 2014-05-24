c965d78e86100024362216fe5e9bd0267981737b
oppo <sohozhou@163.com>
2014-05-24 15:35:32 +0800

    简单的layout布局  验证一下

---
+++ src/com/example/trlab/selfview/ReViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.trlab.selfview;
+
+import com.example.trlab.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReViewActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.re_layout);
+	}
+	
+}
