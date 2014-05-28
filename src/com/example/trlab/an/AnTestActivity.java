b926b48c314a9891e6447395f801800514abd2fe
oppo <sohozhou@163.com>
2014-05-28 11:05:13 +0800

    简单的动画测试  循环变换角度

---
+++ src/com/example/trlab/an/AnTestActivity.java
@@ -0,0 +1,26 @@
+package com.example.trlab.an;
+
+import com.example.trlab.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class AnTestActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.an_test_layout);
+		
+		ImageView im = (ImageView) findViewById(R.id.an_image);
+		Animation anim = AnimationUtils.loadAnimation(this, R.anim.myanim);
+		im.setAnimation(anim);
+//		anim.setRepeatMode(Animation.RESTART);
+//		anim.setRepeatCount(Animation.INFINITE);
+		anim.start();
+	}
+	
+}
