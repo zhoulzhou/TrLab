f54e6a374da7cfa65a8c8fa36e2e23446f6f5f81
oppo <sohozhou@163.com>
2014-09-12 18:24:46 +0800

    listview test start

--- src/com/example/trlab/test/TestData.java
+++ src/com/example/trlab/test/TestData.java
@@ -1,7 +1,126 @@
 package com.example.trlab.test;
 
-public class TestData{
-    public String name;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestData {
     public String picUrl;
+    public String name;
     public String date;
+    public int type;
+    public List<Child> childList = new ArrayList<Child>();
+    public int size;
+    
+    public List<Child> getProductList() {
+        return childList;
+    }
+
+    public int getProductCount() {
+        return size;
+    }
+
+    public Child getProduct(int position) {
+        return childList.get(position);
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+
+    public int getResType() {
+        return type;
+    }
+
+    public class Child {
+        public String picUrl;
+        public String name;
+        public String date;
+        public int type;
+
+        public int getAppType() {
+            return type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPicUrl(int i) {
+            return picUrl;
+        }
+    }
+    
+    
+    private static final int TYPE_THEME = 0;
+    private static final int TYPE_WALLPAPER = 0;
+    private static final int TYPE_LOCK = 0;
+    private static final int TYPE_DYNAMIC_WALLPAPER = 0;
+    private static final int TYPE_FONT = 0;
+    
+    public static List<TestData> initData(){
+        List<TestData> dataList = new ArrayList<TestData>();
+        TestData data1 = new TestData();
+        data1.name = "theme title";
+        data1.type = TYPE_THEME;
+        for(int i=0 ; i<4; i++){
+           TestData.Child child = data1.new Child();
+           child.name = "item " + i;
+           child.type = TYPE_THEME;
+           data1.childList.add(child);
+        }
+        data1.size = data1.childList.size();
+        
+        TestData data2 = new TestData();
+        data2.name = "wallpaper title";
+        data2.type = TYPE_WALLPAPER;
+        for(int i=0 ; i<2; i++){
+           TestData.Child child = data2.new Child();
+           child.name = "item " + i;
+           child.type = TYPE_WALLPAPER;
+           data2.childList.add(child);
+        }
+        data2.size = data2.childList.size();
+        
+        TestData data3 = new TestData();
+        data3.name = "lock title";
+        data3.type = TYPE_LOCK;
+        for(int i=0 ; i<2; i++){
+           TestData.Child child = data3.new Child();
+           child.name = "item " + i;
+           child.type = TYPE_LOCK;
+           data3.childList.add(child);
+        }
+        data3.size = data3.childList.size();
+        
+        TestData data4 = new TestData();
+        data4.name = "dynamic wallpaper title";
+        data4.type = TYPE_DYNAMIC_WALLPAPER;
+        for(int i=0 ; i<10; i++){
+           TestData.Child child = data4.new Child();
+           child.name = "item " + i;
+           child.type = TYPE_DYNAMIC_WALLPAPER;
+           data4.childList.add(child);
+        }
+        data4.size = data4.childList.size();
+        
+        TestData data5 = new TestData();
+        data5.name = "font title";
+        data5.type = TYPE_FONT;
+        for(int i=0 ; i<6; i++){
+           TestData.Child child = data5.new Child();
+           child.name = "item " + i;
+           child.type = TYPE_FONT;
+           data5.childList.add(child);
+        }
+        data5.size = data5.childList.size();
+        
+        dataList.add(data1);
+        dataList.add(data2);
+        dataList.add(data3);
+        dataList.add(data4);
+        dataList.add(data5);
+        return dataList;
+    }
+
 }
---
+++ src/com/example/trlab/test/TestListAdapter1.java
@@ -0,0 +1,96 @@
+package com.example.trlab.test;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import com.example.trlab.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+class TestListAdapter1 extends BaseAdapter {
+    
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_SEPARATOR = 1;
+    private static final int TYPE_MAX_COUNT = TYPE_SEPARATOR + 1;
+
+    private ArrayList mData = new ArrayList();
+    private LayoutInflater mInflater;
+
+    private TreeSet mSeparatorsSet = new TreeSet();
+
+    public TestListAdapter1() {
+        mInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(final String item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addSeparatorItem(final String item) {
+        mData.add(item);
+        // save separator position
+        mSeparatorsSet.add(mData.size() - 1);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mSeparatorsSet.contains(position) ? TYPE_SEPARATOR : TYPE_ITEM;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_MAX_COUNT;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return (String) mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        int type = getItemViewType(position);
+        System.out.println("getView " + position + " " + convertView + " type = " + type);
+        if (convertView == null) {
+            holder = new ViewHolder();
+            switch (type) {
+                case TYPE_ITEM:
+                    convertView = mInflater.inflate(R.layout.item1, null);
+                    holder.textView = (TextView)convertView.findViewById(R.id.text);
+                    break;
+                case TYPE_SEPARATOR:
+                    convertView = mInflater.inflate(R.layout.item2, null);
+                    holder.textView = (TextView)convertView.findViewById(R.id.textSeparator);
+                    break;
+            }
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+        holder.textView.setText(mData.get(position));
+        return convertView;
+    }
+
+}
+
+public class ViewHolder {
+    public TextView textView;
+}
